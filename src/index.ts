194689abb84d5067216a619e074e51bb2a8bb950
bamboo <nmz89825@gmail.com>
2025-01-07 23:39:04 +0800

    feat: add kv namespace and update worker.

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,37 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+interface Env {
+	open_heart_kv: KVNamespace;
+}
+
+function ensureEmoji(emoji: string) {
+	const segments = Array.from(new Intl.Segmenter().segment(emoji.trim()));
+	const parsedEmoji = segments.length > 0 ? segments[0].segment : null;
+
+	if (/\p{Emoji}/u.test(parsedEmoji!)) {
+		return parsedEmoji;
+	}
+	return null;
+}
+
+// derived from https://gist.github.com/muan/388430d0ed03c55662e72bb98ff28f03
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		if (request.method !== 'POST') return new Response('Wrong Method!');
+
+		const url = new URL(request.url);
+		const id = url.searchParams.get('id');
+		const emoji = ensureEmoji(await request.text());
+
+		if (!id || !emoji) return new Response('not ok', { status: 500 });
+
+		const key = `${id}:${emoji}`;
+
+		// https://developers.cloudflare.com/workers/runtime-apis/kv/
+		const currentCount = Number(await(env['open_heart_kv'].get(key)) || 0);
+		await env['open_heart_kv'].put(key, (currentCount + 1).toString());
+
+		return new Response('ok', { status: 200 });
 	},
 } satisfies ExportedHandler<Env>;
